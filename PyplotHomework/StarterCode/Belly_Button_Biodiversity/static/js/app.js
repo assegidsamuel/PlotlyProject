883963bde358b24dc3273bbd67bf1563587da01a
assegidsamuel <41666029+assegidsamuel@users.noreply.github.com>
2018-12-19 15:41:22 -0500

    Add files via upload

--- PyplotHomework/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ PyplotHomework/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -4,9 +4,9 @@ function buildMetadata(sample) {
 
   // Use `d3.json` to fetch the metadata for a sample
     // Use d3 to select the panel with id of `#sample-metadata`
-    var url = `/metadata/${sample}`;
-    d3.json(url).then(function(sample){
-  
+  var url = `/metadata/${sample}`;
+  d3.json(url).then(function(sample){
+    var sample_metadata = d3.select("#sample-metadata");
     // Use `.html("") to clear any existing metadata
     sample_metadata.html("");
     // Use `Object.entries` to add each key and value pair to the panel
@@ -29,53 +29,54 @@ function buildCharts(sample) {
   // @TODO: Use `d3.json` to fetch the sample data for the plots
   var url = `/samples/${sample}`;
   d3.json(url).then(function(data) {
-
-
-    // @TODO: Build a Pie Chart
-    d3.json(url).then(function(data) {  
-       
-      var sample_values = data.sample_values.slice(0,10);
-      var otu_ids = data.otu_ids.slice(0,10);
-      var otu_labels = data.otu_labels.slice(0,10);
-  
-      var data = [{
-        values: sample_values,
-        labels: otu_ids,
-        hovertext: otu_labels,
-        type: 'pie'
-      }];
-  
-        Plotly.newPlot('pie', data);
-              
-        
+console.log(data);
       // @TODO: Build a Bubble Chart using the sample data
-            var x_values = data.otu_ids;
-            var y_values = data.sample_values;
-            var m_size = data.sample_values;
-            var m_colors = data.otu_ids; 
-            var t_values = data.otu_labels;
-        
-            var trace1 = {
-              x: x_values,
-              y: y_values,
-              text: t_values,
-              mode: 'markers',
-              marker: {
-                color: m_colors,
-                size: m_size
-              } 
-            };
-          
-            var data = [trace1];
-        
-            var layout = {
-              xaxis: { title: "OTU ID"},
-            };
-        
-            Plotly.newPlot('bubble', data, layout);       
+      var x = data.otu_ids;
+      var y = data.sample_values;
+      var size = data.sample_values;
+      var colors = data.otu_ids; 
+      var values = data.otu_labels;
+  
+      var trace1 = {
+        x: x,
+        y: y,
+        text: values,
+        mode: 'markers',
+        marker: {
+          color: colors,
+          size: size
+        } 
+      };
+    
+      var data = [trace1];
+  
+      var layout = {
+        xaxis: { title: "OTU ID"},
+      };
+  
+      Plotly.newPlot('bubble', data, layout);
 
+
+      // @TODO: Build a Pie Chart
+               
     // HINT: You will need to use slice() to grab the top 10 sample_values,
     // otu_ids, and labels (10 each).
+
+    d3.json(url).then(function(data) {
+      var pie_values = data.sample_values.slice(0,10);
+      var pie_labels = data.otu_ids.slice(0,10);
+      var pie_hover = data.otu_labels.slice(0,10);
+  
+      var data = [{
+        values: pie_values,
+        labels: pie_labels,
+        hovertext: pie_hover,
+        type: 'pie'
+      }];
+        Plotly.newPlot('pie', data);
+      });
+    });
+  }
 
 
 function init() {
@@ -106,3 +107,4 @@ function optionChanged(newSample) {
 
 // Initialize the dashboard
 init();
+
